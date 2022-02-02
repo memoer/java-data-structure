dc3d5f7332f5b7a2c3f6df676a5392c31a38c248
memoer <hanjn2842@naver.com>
2022-02-02 15:05:52 +0900

    버블 정렬 구현

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -3,10 +3,29 @@ package sort;
 import util.Algorithm;
 
 public class BubbleSort {
+  public static void swap(boolean isAscending, int[] dataList, int i, int j) {
+    int a = dataList[i];
+    int b = dataList[j];
+    if (isAscending) {
+      if (a > b) {
+        dataList[i] = b;
+        dataList[j] = a;
+      }
+    } else if (a < b) {
+      dataList[i] = b;
+      dataList[j] = a;
+    }
+  }
+
   public static void main(String[] args) {
     Algorithm.job(dataList -> {
-
-    }).run(10);
+      int dataListLength = dataList.length;
+      for (int i = 0; i < dataListLength - 1; i++) {
+        for (int j = 0; j < dataListLength - i - 1; j++) {
+          swap(true, dataList, j, j + 1);
+        }
+      }
+    }).run();
   }
 
 }
