dc0bbadb63ff20a55c789240d02ea694905049f6
memoer <hanjn2842@naver.com>
2022-02-03 14:47:38 +0900

    이진 탐색 구현

---
+++ search/BinarySearch.java
@@ -0,0 +1,31 @@
+package search;
+
+import java.util.Arrays;
+
+import util.RandomList;
+
+public class BinarySearch {
+  public static int search(int[] dataList, int search) {
+    int findIdx = -1;
+    int start = 0;
+    int end = dataList.length;
+    Arrays.sort(dataList);
+    while (start < end) {
+      int mid = (start + end) / 2;
+      if (search < dataList[mid]) {
+        end = mid - 1;
+      } else if (search > dataList[mid]) {
+        start = mid + 1;
+      } else {
+        findIdx = mid;
+        break;
+      }
+    }
+    return findIdx;
+  }
+
+  public static void main(String[] args) {
+    int[] dataList = { 69, 65, 18, 71, 11, 10, 56, 52, 66, 89 };
+    System.out.println(search(dataList, 66));
+  }
+}
