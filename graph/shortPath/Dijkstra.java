ab795354cee81ebe2978a013c2ca748afe007b34
memoer <hanjn2842@naver.com>
2022-02-24 23:30:12 +0900

    다익스트라 알고리즘 잘못된 점 고침
    - while반복문에서 if 문 분기처리 추가

--- graph/shortPath/Dijkstra.java
+++ graph/shortPath/Dijkstra.java
@@ -1,12 +1,10 @@
 package graph.shortPath;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Set;
-import java.util.Map.Entry;
 
 import util.Print;
 
@@ -33,27 +31,21 @@ public class Dijkstra {
 
   public static void main(String[] args) {
     Map<Character, List<Node>> graph = new Data().getGraph();
-    Map<Character, Integer> path = getWeightMap(graph.keySet());
+    Map<Character, Integer> distances = getWeightMap(graph.keySet());
     PriorityQueue<Node> pq = getQueue(graph);
     while (!pq.isEmpty()) {
-      for (Entry<Character, Integer> v : path.entrySet()) {
-        System.out.print("(" + v.getKey() + ", " + v.getValue() + "), ");
-      }
-      System.out.println();
-      for (Object node : pq.stream().sorted((pre, cur) -> pre.weight - cur.weight).toArray()) {
-        System.out.print(node + ", ");
-      }
-      System.out.println();
-      System.out.println("------");
       Node cur = pq.poll();
+      if (distances.get(cur.name) < cur.weight) {
+        continue;
+      }
       for (Node adjNode : graph.get(cur.name)) {
         int sum = cur.weight + adjNode.weight;
-        if (sum < path.get(adjNode.name)) {
-          path.put(adjNode.name, sum);
+        if (sum < distances.get(adjNode.name)) {
+          distances.put(adjNode.name, sum);
           pq.add(new Node(adjNode.name, sum));
         }
       }
     }
-    Print.out(path);
+    Print.out(distances);
   }
 }
