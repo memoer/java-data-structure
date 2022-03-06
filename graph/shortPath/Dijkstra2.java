58394dd52b149a208c7f70ad8ee8180e4ab2d4d5
memoer <hanjn2842@naver.com>
2022-03-06 23:43:22 +0900

    최소 신장 트리 - 다익스트라 알고리즘 정확하게
    - 그동안 잘못된 다익스트라 알고리즘 풀이였음

--- graph/shortPath/Dijkstra2.java
+++ graph/shortPath/Dijkstra2.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 public class Dijkstra2 {
+
   public static void main(String[] args) {
     final char START_NODE = 'A';
     Data data = new Data();
@@ -12,20 +13,21 @@ public class Dijkstra2 {
     PriorityQueue<Node> pq = new PriorityQueue<>();
 
     for (char node : data.getGraph().keySet()) {
-      if (node == START_NODE) {
-        path.put(node, 0);
-      } else {
-        path.put(node, Integer.MAX_VALUE);
-      }
+      path.put(node, Integer.MAX_VALUE);
     }
+    path.put(START_NODE, 0);
+
     pq.offer(new Node(START_NODE, 0));
     while (!pq.isEmpty()) {
-      Node curNode = pq.poll();
-      for (Node adjNode : data.getGraph().get(curNode.name)) {
-        int sum = path.get(curNode.name) + adjNode.weight;
-        if (sum < path.get(adjNode.name)) {
-          pq.offer(adjNode);
-          path.put(adjNode.name, sum);
+      Node cur = pq.poll();
+      if (cur.weight > path.get(cur.name)) {
+        continue;
+      }
+      for (Node adj : data.getGraph().get(cur.name)) {
+        int sum = cur.weight + adj.weight;
+        if (sum < path.get(adj.name)) {
+          path.put(adj.name, sum);
+          pq.offer(new Node(adj.name, sum));
         }
       }
     }
@@ -34,4 +36,5 @@ public class Dijkstra2 {
       System.out.println(node + " -> " + path.get(node));
     }
   }
+
 }
